5c4e72b9b1e30070220d29943b36fb069056ce42
magnusld <magnus.dahlo@bekk.no>
2025-08-27 11:08:58 +0200

    Setup simple node environment with Prisma.js and Express.js

---
+++ example-apps/node-express-prisma/index.js
@@ -0,0 +1,16 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', async (req, res) => {
+  res.send('Hello World!');
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
