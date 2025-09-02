44bce30ecca45d243f8947d53a0b72fe7933c9f1
Nikolas Rølland Hugsted <13964858+nikolasrh@users.noreply.github.com>
2025-09-02 19:56:28 +0200

    Add database user/schema for java-spring-jpa

--- docker/oracle/init.sql
+++ docker/oracle/init.sql
@@ -1,20 +1,25 @@
 ALTER SESSION SET CONTAINER=FREEPDB1;
 
--- EF Core
 CREATE USER efcore_user IDENTIFIED BY password;
 GRANT CREATE SESSION TO efcore_user;
+GRANT CREATE TABLE TO efcore_user;
 
--- Dapper/FluentMigrator
-CREATE USER dapper_user IDENTIFIED BY password;
 CREATE USER fluentmigrator_user IDENTIFIED BY password;
 GRANT CREATE SESSION TO fluentmigrator_user;
+GRANT RESOURCE TO fluentmigrator_user;
+
+CREATE USER dapper_user IDENTIFIED BY password;
 GRANT CREATE SESSION TO dapper_user;
 GRANT SELECT ANY TABLE, INSERT ANY TABLE, UPDATE ANY TABLE, DELETE ANY TABLE TO dapper_user;
 
--- Node.js/Express/Prisma
 CREATE USER prisma_user IDENTIFIED BY password;
 GRANT CREATE SESSION TO prisma_user;
+GRANT RESOURCE TO prisma_user;
 
--- Django
 CREATE USER django_user IDENTIFIED BY password;
 GRANT CREATE SESSION TO django_user;
+GRANT RESOURCE TO django_user;
+
+CREATE USER java_spring_jpa_user IDENTIFIED BY password;
+GRANT CREATE SESSION TO java_spring_jpa_user;
+GRANT RESOURCE TO java_spring_jpa_user;
