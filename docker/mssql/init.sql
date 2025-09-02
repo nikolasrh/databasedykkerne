279ca736aab5a9041e256bba5d2bce0e68c5ef9e
Nikolas Rølland Hugsted <13964858+nikolasrh@users.noreply.github.com>
2025-09-02 19:56:28 +0200

    Add database user/schema for java-spring-jpa

--- docker/mssql/init.sql
+++ docker/mssql/init.sql
@@ -1,4 +1,3 @@
--- EF Core
 CREATE DATABASE efcore_db;
 GO
 USE efcore_db;
@@ -8,7 +7,6 @@ CREATE USER efcore_user FOR LOGIN efcore_user;
 ALTER ROLE db_owner ADD MEMBER efcore_user;
 GO
 
--- Dapper/FluentMigrator
 CREATE DATABASE dapper_fluentmigrator_db;
 GO
 USE dapper_fluentmigrator_db;
@@ -23,7 +21,6 @@ CREATE USER fluentmigrator_user FOR LOGIN fluentmigrator_user;
 ALTER ROLE db_owner ADD MEMBER fluentmigrator_user;
 GO
 
--- Node.js/Express/Prisma
 CREATE DATABASE node_express_prisma_db;
 GO
 USE node_express_prisma_db;
@@ -33,7 +30,6 @@ CREATE USER prisma_user FOR LOGIN prisma_user;
 ALTER ROLE db_owner ADD MEMBER prisma_user;
 GO
 
--- Django
 CREATE DATABASE django_db;
 GO
 USE django_db;
@@ -41,4 +37,13 @@ GO
 CREATE LOGIN django_user WITH PASSWORD = 'Password123!';
 CREATE USER django_user FOR LOGIN django_user;
 ALTER ROLE db_owner ADD MEMBER django_user;
+GO
+
+CREATE DATABASE java_spring_jpa_db;
+GO
+USE java_spring_jpa_db;
+GO
+CREATE LOGIN java_spring_jpa_user WITH PASSWORD = 'Password123!';
+CREATE USER java_spring_jpa_user FOR LOGIN java_spring_jpa_user;
+ALTER ROLE db_owner ADD MEMBER java_spring_jpa_user;
 GO
--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -1,9 +1,7 @@
--- EF Core
 CREATE DATABASE efcore_db;
 CREATE USER efcore_user WITH PASSWORD 'password';
 GRANT ALL PRIVILEGES ON DATABASE efcore_db TO efcore_user;
 
--- Dapper/FluentMigrator
 CREATE DATABASE dapper_fluentmigrator_db;
 CREATE USER dapper_user WITH PASSWORD 'password';
 CREATE USER fluentmigrator_user WITH PASSWORD 'password';
@@ -11,12 +9,14 @@ GRANT ALL PRIVILEGES ON DATABASE dapper_fluentmigrator_db TO fluentmigrator_user
 GRANT CONNECT ON DATABASE dapper_fluentmigrator_db TO dapper_user;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO dapper_user;
 
--- Node.js/Express/Prisma
 CREATE DATABASE node_express_prisma_db;
 CREATE USER prisma_user WITH PASSWORD 'password';
 GRANT ALL PRIVILEGES ON DATABASE node_express_prisma_db TO prisma_user;
 
--- Django
 CREATE DATABASE django_db;
 CREATE USER django_user WITH PASSWORD 'password';
 GRANT ALL PRIVILEGES ON DATABASE django_db TO django_user;
+
+CREATE DATABASE java_spring_jpa_db;
+CREATE USER java_spring_jpa_user WITH PASSWORD 'password';
+GRANT ALL PRIVILEGES ON DATABASE java_spring_jpa_db TO java_spring_jpa_user;
