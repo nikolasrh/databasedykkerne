df4c09538613ddaaf901b3515fab9130d1e24166
Endre Waatevik <endwaat@gmail.com>
2025-09-04 17:32:55 +0200

    Oppdaterer connection string med korrekt passord

--- example-apps/dotnet-efcore/EFCoreApp.Tests/DatabaseConnectionTests.cs
+++ example-apps/dotnet-efcore/EFCoreApp.Tests/DatabaseConnectionTests.cs
@@ -21,7 +21,7 @@ public class DatabaseConnectionTests
     [Fact(Timeout = 1000, Skip = "Database disabled")]
     public async Task TestPostgreSqlConnection()
     {
-        var connectionString = "Host=host.docker.internal;Port=5432;Database=efcore_db;Username=efcore_user;Password=Password123!;";
+        var connectionString = "Host=host.docker.internal;Port=5432;Database=efcore_db;Username=efcore_user;Password=password;";
         var options = new DbContextOptionsBuilder<PersonContext>()
             .UseNpgsql(connectionString)
             .Options;
